882d405a6a8410eba2c42a5509342ba97bd4ef04
kimberly <leislalisilva@gmail.com>
2024-08-16 09:36:43 -0300

    agregar archivo

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+
+function comprobarClave() {
+    let clave1 = document.getElementById("password1").value
+    let clave2 = document.getElementById("password2").value
+
+    if ( password1 == password2) {
+        if ( == password2) {
+       alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo")
+    } else {
+       alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo")
+    }
+}}
