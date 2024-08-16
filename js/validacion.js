cec19126eb9d17f9241abbe8000afa6497203941
Sofia <sofiacarrion2815@gmail.com>
2024-08-16 11:37:28 -0300

    carga

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,14 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function checkPassword() {
+    let password1 = document.getElementById("password1").value 
+    let password2 = document.getElementById("password2").value 
+    if (password1 != password2) {
+        alert ("Las contraseñas no coinciden")
+
+    } else {
+        alert ("Las contraseñas coinciden")
+    }
+}
